c4a9c8c05758a9a9098564dcb52f1af86fee4bb6
paleonluna <paleonluna@gmail.com>
2015-06-03 15:12:30 -0700

    GUIManager added and shake added

---
+++ Assets/Shake.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shake : MonoBehaviour {
+	
+		
+	//This script takes a UI image and shakes it around in 2D directions to make look cool
+	
+	public float shakeAmount = 50f;
+	float shakeSpeed = .025f;
+	bool isShaking;
+	public float shakeTime = 1f;
+	float timeAtEnd;
+
+	void Start () {
+		StartShake ();
+	}
+
+	public void StartShake()
+	{
+		timeAtEnd = Time.time;
+		isShaking = true;
+		StartCoroutine ("ShakeImage");
+	}
+
+	//I could refactor this to use SIN function
+	IEnumerator ShakeImage()
+	{
+		while (isShaking)
+		{
+//			float xAmount = (float)Random.Range(-shakeAmount, shakeAmount);
+//			float yAmount = (float)Random.Range(-shakeAmount, shakeAmount);
+			gameObject.transform.Translate(new Vector3(shakeAmount, 0, 0), gameObject.transform);
+			yield return new WaitForSeconds(shakeSpeed);
+			gameObject.transform.Translate(new Vector3(-shakeAmount, -0, 0), gameObject.transform);
+			yield return new WaitForSeconds(shakeSpeed);
+		}
+	}	
+
+	void Update () {
+		if (Time.time > shakeTime + timeAtEnd) {
+			isShaking = false;
+		}
+	}
+}
